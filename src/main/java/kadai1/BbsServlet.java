9894ed5dd620d6d9244fa284b7461adf50e5af6a
785-student017 <785-student017@la-study.com>
2026-06-02 15:15:24 +0900

    jm10-step02

--- src/main/java/kadai1/BbsServlet.java
+++ src/main/java/kadai1/BbsServlet.java
@@ -1,10 +1,51 @@
 package kadai1;
 
-public class BbsServlet {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+import java.io.IOException;
+import java.io.PrintWriter;
 
-	}
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet("/BbsServlet")
+public class BbsServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<title>掲示板</title>");
+        out.println("</head>");
+        out.println("<body>");
+
+        out.println("メッセージ：<br>");
+
+        out.println("<form action='/jmaster/BbsServlet' method='post'>");
+        out.println("<textarea name='message' style='width:300px; height:100px;'></textarea><br>");
+        out.println("<button type='submit'>書き込み</button>");
+        out.println("</form>");
+
+        out.println("<hr>");
+
+        out.println("</body>");
+        out.println("</html>");
+
+        out.close();
+    }
 }
